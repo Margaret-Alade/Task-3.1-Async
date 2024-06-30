77c19396ecedbbae3aea8c4a56486b748fc79fb6
Margaret Alade <alade.margaret@yandex.ru>
2024-06-30 18:28:02 +0300

    Initial Commit

---
+++ Task-3.1-Async/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Task-3.1-Async
+//
+//  Created by Маргарет  on 30.06.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
