319855a027ef7e8c785d8b0a9ec328f8f916674a
Margaret Alade <alade.margaret@yandex.ru>
2024-07-03 07:20:31 +0300

    Correct HW

--- Task-3.1-Async/main.cpp
+++ Task-3.1-Async/main.cpp
@@ -8,8 +8,9 @@
 #include <iostream>
 #include <future>
 #include <vector>
+#include <climits>
 
-int min_num(int *arr, int size, std::promise<int> & pr) {
+void sort_arr(int *arr, int size) {
     int i, j, min;
     for (i = 0; i < size - 1; i++) {
         min = i;
@@ -24,17 +25,25 @@ int min_num(int *arr, int size, std::promise<int> & pr) {
         }
         
     }
+}
+
+int min_el(int *arr, int size,int start_i, std::promise<int> &pr) {
+    int min = INT_MAX;
     
-    pr.set_value(arr[0]);
-    return arr[0];
+    for (int i = start_i; i < size; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    
+    pr.set_value(min);
+    return min;
 }
 
 int main(int argc, const char * argv[]) {
-    std::promise<int> pr;
-    std::future<int> ft = pr.get_future();
-    
     const int size = 10;
-    int arr[size] = {4, 2 , 10, 5, 1, 0, 8, 3, 11, 20};
+    int arr[size] = {4, 2 , 10, 5, 4, 12, 8, 3, 11, 20};
+    
     std::cout << "Неотсортированный массив: ";
     
     for(int i = 0; i < size; i++) {
@@ -42,13 +51,18 @@ int main(int argc, const char * argv[]) {
     }
     std::cout << "\n";
     
-    auto res = std::async(min_num, std::ref(arr), size, std::ref(pr));
-    ft.wait();
-    auto min = ft.get();
+    sort_arr(arr, size);
+    
     std::cout << "Отсортированный массив: ";
     for(int i = 0; i < size; i++) {
         std::cout << arr[i] << " ";
     }
+    
+    std::promise<int> pr;
+    std::future<int> ft = pr.get_future();
+    auto res = std::async(min_el, std::ref(arr), size, 0, std::ref(pr));
+    ft.wait();
+    auto min = ft.get();
     std::cout << "\n";
     std::cout << "Наименьший элемент: " << min << "\n";
     
